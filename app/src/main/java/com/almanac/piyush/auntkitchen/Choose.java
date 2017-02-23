3558dc3e032441887a633123d5348151e83e7f67
piyush189@gmail.com <Apogee189@>
2017-02-23 23:25:15 +0530

    Initial commit

---
+++ app/src/main/java/com/almanac/piyush/auntkitchen/Choose.java
@@ -0,0 +1,21 @@
+package com.almanac.piyush.auntkitchen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Choose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void auntyclick(View view){
+        startActivity(new Intent(this,auntlogin.class));
+    }
+    public void customerclick(View view){
+        startActivity(new Intent(this,custlogin.class));
+    }
+}
